570cf519d7a96f08f280daf5270294736881c5a1
Davide Lo Re <davide@camper.localdomain>
2009-02-24 10:58:13 +0100

    package_bar/plugin.py now is more similar to how it is intended to be

--- package_bar/plugin.py
+++ package_bar/plugin.py
@@ -1,1 +1,9 @@
-print 'plugin loaded'
+import extension
+import bar
+
+class Plugin:
+	def __init__(self):
+		pass
+	def extensions_register(self):
+		print bar.Bar
+		extension.register('bar_category', bar.Bar)
