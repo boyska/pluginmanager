b02e97d6d5972cfb0fb69fcf16bbaa7c52b0638e
Davide Lo Re <davide@camper.localdomain>
2009-02-24 12:36:00 +0100

    Added "Package", class that abstract a package
    It will be useful to treat plugins and packages the same way.
    At the moment it has start(), stop(), instanciate() and is_active().

--- import_package.py
+++ import_package.py
@@ -5,15 +5,65 @@ for root, d, files in os.walk('.'):
     break #SOOOO UGLY!
 
 plugins = {}
+
+class Package:
+	'''Abstraction over a package.
+	Will be useful to work in the same way with plugins and packages.
+	'''
+	def __init__(self, directory):
+		'''@param directory The directory containing the package'''
+		self.name = directory
+		self.module = __import__(directory, globals(), None, ['plugin'])
+		self.instance = None #we are not instancing it
+
+	def instanciate(self):
+		'''Instanciate (if not already done). 
+		You shouldn't need this, but you can use it for performance tweak.
+		'''
+		if self.instance:
+			return self.instance
+		try:
+			self.instance = self.module.plugin.Plugin()
+		except:
+			self.instance = None
+
+		return self.instance
+
+	def start(self):
+		'''Instanciate (if necessary) and starts the plugin.
+		@return False if something goes wrong, else True.
+		'''
+		inst = self.instanciate()
+		if not inst:
+			return False
+		try:
+			inst.start()
+		except:
+			return False
+		return True
+				
+	def stop(self):
+		if self.active:
+			self.instance.stop()
+	
+	def is_active(self):
+		if not self.instance:
+			return False
+		return True
+		return self.instance.active
+
+
 for directory in [x for x in dirs if not x.startswith('.')]:
     print 'D', directory
     
     try:
-        mod = __import__(directory, globals(), None, ['plugin'])
+        #mod = __import__(directory, globals(), None, ['plugin'])
+        mod = Package(directory)
     except Exception, e:
         print 'Exception while importing %s:\n%s' % (directory, e)
 
     plugins[directory] = mod
 
-print plugins
+for package in plugins.values():
+	print package.name, package.is_active()
 
