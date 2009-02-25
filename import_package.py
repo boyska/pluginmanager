70af3facfaf5d8e7dbb445fcbb3cd3d558285a5a
Davide Lo Re <davide@camper.localdomain>
2009-02-25 02:13:24 +0100

    Plugin and Package changed their names
    Now they are called PluginHandler and PackageHandler

--- import_package.py
+++ import_package.py
@@ -2,14 +2,77 @@ import os
 import sys
 
 
-class Package:
+class PluginHandler:
+    def __init__(self, filename):
+        self.name = filename.split('.')[0] #TODO basename
+        self.module = None
+        self._instance = False
+        self._do_import()
+
+    def _do_import(self):
+        '''Does the dirty stuff with __import__'''
+        old_syspath = sys.path
+        try:
+            sys.path = [os.curdir, 'plugins']
+            self.module = __import__(self.name, globals(), locals(), ['plugin'])
+        except ImportError, e:
+            print 'error loading', self.name
+            print '-->', e
+        finally:
+            sys.path = old_syspath
+
+    def instanciate(self):
+        '''Instanciate (if not already done). 
+
+        You shouldn't need this, but you can use it for performance tweak.
+
+        '''
+        if self.instance:
+            return self.instance
+        try:
+            self.instance = self.module.Plugin()
+        except:
+            self.instance = None
+
+        return self.instance
+
+    def start(self):
+        '''Instanciate (if necessary) and starts the plugin.
+        @return False if something goes wrong, else True.
+        '''
+        self.instanciate()
+        if not self.instance:
+            return False
+        try:
+            self.instance.start()
+        except:
+            print "errore nello start di", self.name
+            return False
+        return True
+            
+    def stop(self):
+        '''Stop the plugin, of course'''
+        if self.active:
+            self.instance.stop()
+
+    def is_active(self):
+        '''@return True if an instance exist and is started. False otherwise'''
+        if not self._instance:
+            return False
+        return self._instance.is_active()
+
+class PackageHandler:
     '''Abstraction over a package.
     Will be useful to work in the same way with plugins and packages.
     '''
     def __init__(self, directory):
         '''@param directory The directory containing the package'''
         self.name = directory
-        print os.listdir(os.curdir)
+        self.instance = False #we are not instancing it
+        self._do_import()
+
+    def _do_import(self):
+        '''Does the dirty stuff with __import__'''
         old_syspath = sys.path
         try:
             sys.path = ['.', 'plugins']
@@ -20,7 +83,6 @@ class Package:
             self.module = None
         finally:
             sys.path = old_syspath
-        self.instance = False #we are not instancing it
 
     def instanciate(self):
         '''Instanciate (if not already done). 
@@ -54,57 +116,7 @@ class Package:
             self.instance.stop()
 
     def is_active(self):
-        if not self.instance:
-            return False
-        return self.instance.is_active()
-
-class Plugin:
-    def __init__(self, filename):
-        self.name = filename.split('.')[0] #TODO basename
-        self.module = None #__import__ blablabla
-        old_syspath = sys.path
-        try:
-            sys.path = [os.curdir, 'plugins']
-            self.module = __import__(self.name, globals(), locals(), [])
-        except ImportError, e:
-            print 'error loading', self.name
-            print '-->', e
-        finally:
-            sys.path = old_syspath
-        self.instance = False
-
-    def instanciate(self):
-        '''Instanciate (if not already done). 
-        You shouldn't need this, but you can use it for performance tweak.
-        '''
-        if self.instance:
-            return self.instance
-        try:
-            self.instance = self.module.Plugin()
-        except:
-            self.instance = None
-
-        return self.instance
-
-    def start(self):
-        '''Instanciate (if necessary) and starts the plugin.
-        @return False if something goes wrong, else True.
-        '''
-        self.instanciate()
-        if not self.instance:
-            return False
-        try:
-            self.instance.start()
-        except:
-            print "errore nello start di", self.name
-            return False
-        return True
-            
-    def stop(self):
-        if self.active:
-            self.instance.stop()
-
-    def is_active(self):
+        '''@return True if an instance exist and is started. False otherwise'''
         if not self.instance:
             return False
         return self.instance.is_active()
@@ -116,19 +128,18 @@ for root, d, f in os.walk('plugins'):
     break #SOOOO UGLY!
 
 plugins = {}
-print 'FILES', files
 
 
 for directory in [x for x in dirs if not x.startswith('.')]:
     try:
-        mod = Package(directory)
+        mod = PackageHandler(directory)
         plugins[directory] = mod
     except Exception, e:
         print 'Exception while importing %s:\n%s' % (directory, e)
 
 for file in [x for x in files if x.endswith('.py')]:
     try:
-        mod = Plugin(file)
+        mod = PluginHandler(file)
         plugins[directory] = mod
     except Exception, e:
         print 'Exception while importing %s:\n%s' % (directory, e)
@@ -138,7 +149,7 @@ for package in plugins.values():
     print package.name, package.is_active()
 
 for package in plugins.values():
-    print package.name, package.is_active(),
+    print package.name, package.is_active(), '-->',
     package.start()
     print package.is_active(), package.module
 
--- plugins/foobar.py
+++ plugins/foobar.py
@@ -1,4 +1,7 @@
-class Plugin:
+import plugin
+
+class Plugin(plugin.Plugin):
 	def __init__(self):
+		plugin.Plugin.__init__(self)
 		pass
 
