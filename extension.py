681cc96b02dc1c486374238a120e218739bf3625
Davide Lo Re <davide@camper.localdomain>
2009-03-03 00:23:24 +0100

    Added extension.get_default
    ATM it will just return get_extensions(category)[0], but we should choose
    by preferences there.
    Small refactoring on category_register and register

--- extension.py
+++ extension.py
@@ -3,45 +3,50 @@ _classes = {} #'ComponentName': <class 'Component'>
 _instances = {} #'ComponentName': <Component object>
 
 def category_register(category):
-	'''Add a category to the registry;
-	TODO: find a way to "raise a warning".
-	'''
-	if not category in _registry:
-		print "Warning: %s doesn't exist" % (category)
-	_registry[category] = []
+    '''Add a category to the registry'''
+    _registry[category] = []
 
 def register(category, cls):
-	'''Register cls as an Extension for category. 
-	If the category doesn't exist, it creates it(but returns False).
-	It doesn't instanciate that class immediately
-	@return False if the category didn't exist. Probably you made a mistake, True otherwise.
-	'''
-	class_name = _get_class_name(cls)
-	_classes[class_name] = cls
+    '''Register cls as an Extension for category. 
+    If the category doesn't exist, it creates it(but returns False).
+    It doesn't instanciate that class immediately
+    @return False if the category didn't exist. Probably you made a mistake, True otherwise.
+    '''
+    class_name = _get_class_name(cls)
+    _classes[class_name] = cls
 
-	try:
-		_registry[category].append(class_name)
-	except KeyError:
-		_registry[category] = [class_name]
-		return False #check this: probably that category doesn't exist (or it's not well-declared)
-	
-	return True
-	#note: we shouldn't immediately instanciate it
+    try:
+        _registry[category].append(class_name)
+    except KeyError:
+        category_register(category)
+        _registry[category].append(class_name)
+        return False #check this: probably that category doesn't exist (or it's not well-declared)
+    
+    return True
+    #note: we shouldn't immediately instanciate it
 
 def get_extensions(category):
-	'''return a list of ready-to-use extension instances'''
-	return [_instance_of(class_name) for class_name in _registry[category]]
+    '''return a list of ready-to-use extension instances'''
+    return [_instance_of(class_name) for class_name in _registry[category]]
+
+def get_default(category)::
+	'''return ONE extension instance. It will be chosen with preferences'''
+	#put here your choosing logic (preferences)
+	available = get_extensions(category)
+	if available:
+		return available[0] #that's just a test: you should choose better
+	return None
 
 def _instance_of(class_name):
-	'''Given a class name, will return a ready-to-use instance. 
-	Every "trick" (hey, only if necessary), will be done here.
-	'''
-	if class_name in _instances:
-		return _instances[class_name]
+    '''Given a class name, will return a ready-to-use instance. 
+    Every "trick" (hey, only if necessary), will be done here.
+    '''
+    if class_name in _instances:
+        return _instances[class_name]
 
-	instance = _classes[class_name]()
-	_instances[class_name] = instance
-	return instance
+    instance = _classes[class_name]()
+    _instances[class_name] = instance
+    return instance
 
 def _get_class_name(cls):
     '''Returns the full name of a class: module.class
